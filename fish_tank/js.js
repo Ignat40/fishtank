c9e07e57ed5fe67e47064a419198185f24f7ab04
Ignat Bojinov <ignatbojinov@gmail.com>
2022-05-11 19:16:25 +0300

    added animation

--- fish_tank/js.js
+++ fish_tank/js.js
@@ -8,24 +8,34 @@ const numberOfEnemies = Math.floor(Math.random() * 10) + 1;
 const enemiesArray = [];
 
 
-const enemyImage = new Image();
-enemyImage.src = 'photos/ribka.png';
+// const enemyImage = new Image();
+// enemyImage.src = 'photos/ribka.png';
 
 class Enemy{
     constructor(){
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.image = new Image();
+        this.image.src = 'photos/ribka.png'
         this.width = 100;
         this.height = 100;
-        this.speed = Math.random() * 4 - 2;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.speed = Math.random() * 4 + 1;
+        this.angle = Math.random() * 500;
+        this.angleSpeed = Math.random() * 1.5  + 0.5;
+        //this.curve = Math.random() * 200 + 50;
+        
     }
     update(){
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x = canvas.width/5 * Math.cos(this.angle * Math.PI/200) + (canvas.
+        width/2 - this.width/2);
+        this.y =  canvas.height/5 * Math.sin(this.angle * Math.PI/300) + (canvas.
+        height/2 - this.height/2);;
+        this.angle += this.angleSpeed;
+        // this.y += this.speed;
+        if(this.x + this.width < 0) this.x = canvas.width;
     };
     draw(){
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 };
 
